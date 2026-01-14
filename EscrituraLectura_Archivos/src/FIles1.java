5274f8fda5a5e8e9c62b4bfc2cc5fc6c92c18495
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2026-01-14 17:32:22 -0500

    Subiendo proyecto

--- EscrituraLectura_Archivos/src/FIles1.java
+++ EscrituraLectura_Archivos/src/FIles1.java
@@ -1,6 +1,25 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class FIles1 {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        //FLUJO DE SALIDA, OUPUT, ESCRITURA, PERSISTENCIA
+        //Formatter nomVariable = new Formatter(new File("nombreArch.txt"));
+        try {
+            Formatter arcOut = new Formatter(new File("Saludo.csv"));
+            arcOut.format("%s;%d", "Hola mundo", 10);
+            arcOut.close();
+            Scanner arcIn = new Scanner(new File("Saludo.txt"));
+            Scanner arcInNotas = new Scanner(new File("datos.csv"));
+            System.out.println(arcInNotas.nextLine());
+            System.out.println(arcInNotas.nextLine());
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        
+        
     }
 }
