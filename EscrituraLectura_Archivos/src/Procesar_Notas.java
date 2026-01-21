cfbacdd4894abda67821e30179fa565130999a09
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2026-01-21 16:30:43 -0500

    act

--- EscrituraLectura_Archivos/src/Procesar_Notas.java
+++ EscrituraLectura_Archivos/src/Procesar_Notas.java
@@ -1,3 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Procesar_Notas {
+    public static void main(String[] args) {
+        String datosNotas [][] = LeerArchivo_Matriz("NotasIn.csv", 11, 6);
+    }
     
+    public static String [][] LeerArchivo_Matriz(String nombreArchivo, int m, int n){
+        String datos[][] = new String[m][n];
+        try {
+            Scanner fin = new Scanner(new File(nombreArchivo));
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return datos;
+    }
 }
